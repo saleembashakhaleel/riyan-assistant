ab5d363c5aaf9f6e26b74834f9336a7697dad7e0
saleembashakhaleel <saleem1490@gmail.com>
2026-02-16 01:24:05 +0530

    Add files via upload

--- assistant.py
+++ assistant.py
@@ -276,13 +276,10 @@ def main():
     print("Riyan Jarvis Cloud Brain Activated...")
     print("🧠 Starting Jarvis Reminder Engine...")
 
-    app.job_queue.run_repeating(
-        reminder_job,
-        interval=60,
-        first=10
-    )
+    app.job_queue.run_repeating(reminder_job, interval=60, first=5)
 
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
